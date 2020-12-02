c65fb5690d873662c3418c5ed06d4b7dd2079f3b
PetarB <bojko94@gmail.com>
2020-12-02 14:04:59 +0100

    starter project Spring Boot

---
+++ MoviesAppSpringBoot/src/main/java/com/petar/MoviesAppSpringBoot/MoviesAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.petar.MoviesAppSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesAppSpringBootApplication.class, args);
+	}
+
+}
